e584f194799320d717ba24e0fbb188f9c26f645f
Flavio Ceolin <flavio@hubblenetwork.com>
2025-02-11 21:36:08 +0000

    TEMP mmahadevan

--- tests/subsys/pm/power_domain_soc_state_change/src/main.c
+++ tests/subsys/pm/power_domain_soc_state_change/src/main.c
@@ -40,31 +40,6 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 	irq_unlock(0);
 }
 
-
-/* A custom policy manager is used so we can control the Power States
- * to transition to and test the issuance of TURN_ON and TURN_OFF actions
- * by the power domain driver.
- */
-const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
-{
-	const struct pm_state_info *cpu_states;
-
-	pm_state_cpu_get_all(cpu, &cpu_states);
-
-	if (testing_domain_on_notitication == 0) {
-		/* First power state to transition is STANDBY */
-		return &cpu_states[0];
-	} else if (testing_domain_on_notitication == 1) {
-		/* Second power state to transition is SUSPEND-TO-IDLE */
-		return &cpu_states[1];
-	} else if (testing_domain_on_notitication == 2) {
-		/* Third power state to transition is RUNTIME-IDLE */
-		return &cpu_states[2];
-	}
-
-	return NULL;
-}
-
 static int dev_pm_action(const struct device *dev, enum pm_device_action pm_action)
 {
 	ARG_UNUSED(dev);
@@ -85,20 +60,25 @@ DEVICE_DT_DEFINE(TEST_DEV, NULL, PM_DEVICE_DT_GET(TEST_DEV), NULL, NULL, POST_KE
 
 ZTEST(power_domain_soc_state_change_1cpu, test_power_domain_soc_state_change)
 {
+	const struct pm_state_info *cpu_states;
+
+	(void)pm_state_cpu_get_all(0, &cpu_states);
+
 	/* Sleep to transition to first STATE: STANDBY */
-	k_sleep(K_USEC(1));
+	k_usleep(cpu_states[2].min_residency_us + cpu_states[2].exit_latency_us);
 
 	zassert_equal(testing_domain_on_notitication, 1);
 	zassert_equal(testing_domain_off_notitication, 1);
 
+	printk("YAY\n");
 	/* Sleep to transition to second STATE: SUSPEND-TO-IDLE */
-	k_sleep(K_USEC(1));
+	k_usleep(cpu_states[1].min_residency_us + cpu_states[1].exit_latency_us);
 
 	zassert_equal(testing_domain_on_notitication, 2);
 	zassert_equal(testing_domain_off_notitication, 2);
 
 	/* Sleep to transition to second STATE: RUNTIME-IDLE */
-	k_sleep(K_USEC(1));
+	k_usleep(cpu_states[0].min_residency_us + cpu_states[0].exit_latency_us);
 
 	/* The domain notification flags should remain the same as RUNTIME-IDLE
 	 * is not listed as an ON/OFF power state in device-tree.
